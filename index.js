1e4a5a9ebe8e07855de294a3d228956c8cbc7dd4
Naomi Quinones <naomiquinones@email.com>
2018-02-18 04:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
+<<<<<<< HEAD
   return `Happy holidays, ${name}!`;
 }
 
@@ -14,3 +15,11 @@ function happyHolidayTo(holiday,name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
+=======
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday,name) {
+  return `Happy ${holiday}, ${name}`
+}
+>>>>>>> 888abdea244917514e7cda8c38862a405ab45398
